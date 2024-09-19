6e1ab22eb859be0daa139f6b3ee7bf6d7f225207
abubakarafridi <abubakarafridi407@gmail.com>
2024-09-19 21:46:40 +0500

    I have successfully did post request and tested it in postman

--- server.mjs
+++ server.mjs
@@ -1,10 +1,16 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 const PORT = 3000;
 
 app.get('/products', (req, res) => {
   res.status(200).json({ message : 'Get Request - fetching all products' });
+});
+
+app.post('/products', (req, res) => {
+    const newItem = req.body;
+  res.status(201).json({ message : 'Post Request - Posting product detail', data: newItem });
 });
 
 app.listen(PORT, () => {
